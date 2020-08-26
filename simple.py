7e056050950932af620bf5366400eeec0f9d0229
adelrune <guiloua2@hotmail.com>
2020-08-26 17:46:04 -0400

    remembering my too clever for its own good code

--- simple.py
+++ simple.py
@@ -5,8 +5,8 @@ s = Server(audio="jack").boot()
 
 
 osc = OSCToSig(idle_timer=False, port=8000)
-fader1 = osc["/1/fader1"].sig
-fader2 = osc["/1/fader2"].sig
+fader1 = osc[1]["fader1"].sig
+fader2 = osc[1]["fader2"].sig
 
 audio_in = Input()
 dist = Disto(audio_in, drive=fader1, slope=fader2).out()
